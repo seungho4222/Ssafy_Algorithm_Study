9fff27e3445d3166943da0bf3a2f5e2d4c7259a8
seungho <kials1@naver.com>
2023-09-15 22:24:33 +0900

    230915

--- 이승호/week_7/9084_동전.py
+++ 이승호/week_7/9084_동전.py
@@ -1,7 +1,16 @@
+def f():
+    for coin in coins:
+        for i in range(coin, M + 1):
+            dp[i] += dp[i - coin]
+    return dp[M]
+
+
 T = int(input())
 for _ in range(T):
     N = int(input())  # 동전의 가지 수
     coins = list(map(int, input().split()))
     M = int(input())  # 동전으로 만들어야 할 값
-    
-    
+
+    dp = [0] * (M+1)
+    dp[0] = 1
+    print(f())
