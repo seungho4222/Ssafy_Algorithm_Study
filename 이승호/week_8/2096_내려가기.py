bbbf3f432442a152db80a5727b2d6c0f5cc9e5bc
seungho <kials1@naver.com>
2023-09-20 15:08:43 +0900

    230920

---
+++ 이승호/week_8/2096_내려가기.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+max_arr = [0, 0, 0]
+min_arr = [0, 0, 0]
+
+for i in range(N):
+    arr = list(map(int, input().split()))
+    max_arr = [arr[0] + max(max_arr[:2]), arr[1] + max(max_arr), arr[2] + max(max_arr[1:])]
+    min_arr = [arr[0] + min(min_arr[:2]), arr[1] + min(min_arr), arr[2] + min(min_arr[1:])]
+
+print(max(max_arr), min(min_arr))
