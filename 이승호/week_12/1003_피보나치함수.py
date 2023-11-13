ae9ccef7b51516c19c59c38751caa03275ccd352
seungho <kials1@naver.com>
2023-11-13 22:12:10 +0900

    231113

---
+++ 이승호/week_12/1003_피보나치함수.py
@@ -0,0 +1,14 @@
+T = int(input())
+for tc in range(T):
+    N = int(input())
+
+    dp = [[0, 0] for _ in range(41)]
+    dp[0] = [1, 0]
+    dp[1] = [0, 1]
+
+    if N > 1:
+        for i in range(2, N+1):
+            dp[i][0] = dp[i-1][0] + dp[i-2][0]
+            dp[i][1] = dp[i-1][1] + dp[i-2][1]
+
+    print(*dp[N])
